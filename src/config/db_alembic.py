876769549fbab2d5b85c97848f4efa822cbf617a
RuslanChukavin <r66cha@gmail.com>
2025-05-06 02:16:33 +0300

    Удалён .env.dev и добавлен в .gitignore

--- src/config/db_alembic.py
+++ src/config/db_alembic.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class SettingsDB_DEV(BaseSettings):
-    DB_HOST_DEV: str
+class SettingsDB_ALEMBIC(BaseSettings):
+    DB_HOST_ORIGINAL: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
@@ -12,12 +12,12 @@ class SettingsDB_DEV(BaseSettings):
     def DB_URL_asyncpg_dev(self) -> str:
         """Возвращает строку подключения к базе данных для asyncpg"""
 
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST_DEV}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST_ORIGINAL}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env.dev", extra="ignore")
-
-
-db_settings_dev = SettingsDB_DEV()
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
-__all__ = ["db_settings_dev"]
+db_settings_alembic = SettingsDB_ALEMBIC()
+
+
+__all__ = ["db_settings_alembic"]
