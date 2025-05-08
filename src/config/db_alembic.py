c955e6f673622b9e2b996f1cacc023fdd5026f6c
RuslanChukavin <r66cha@gmail.com>
2025-05-08 05:05:57 +0300

    .7

--- src/config/db_alembic.py
+++ src/config/db_alembic.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class SettingsDB_ALEMBIC(BaseSettings):
-    DB_HOST_ORIGINAL: str
+    DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
@@ -10,7 +10,7 @@ class SettingsDB_ALEMBIC(BaseSettings):
 
     @property
     def DB_URL_asyncpg_alembic(self) -> str:
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST_ORIGINAL}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(env_file=".env.local", extra="ignore")
 
