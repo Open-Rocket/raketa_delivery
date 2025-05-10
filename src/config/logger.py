a8873352832db940d9a056606820b296542464a5
RuslanChukavin <r66cha@gmail.com>
2025-05-10 09:42:01 +0300

    simple log

--- src/config/logger.py
+++ src/config/logger.py
@@ -1,24 +1,43 @@
+# import logging
+
+# # ANSI-коды цветов
+# COLORS = {
+#     "RESET": "\033[0m",  # Сброс цвета
+#     "DEBUG": "\033[94m",  # Синий
+#     "INFO": "\033[30m",  # сСерый
+#     "WARNING": "\033[93m",  # Жёлтый
+#     "ERROR": "\033[91m",  # Красный
+#     "CRITICAL": "\033[41m",  # Красный фон
+# }
+
+
+# class ColoredFormatter(logging.Formatter):
+#     def format(self, record) -> str:
+#         """Форматирование лог-записи с учётом цвета"""
+
+#         log_color = COLORS.get(record.levelname, COLORS["RESET"])
+#         log_message = super().format(record)
+#         return f"{log_color}{log_message}{COLORS['RESET']}"
+
+
+# # Настраиваем логгер
+# log = logging.getLogger()
+# log.setLevel(logging.INFO)
+
+# # Хэндлер для консоли
+# handler = logging.StreamHandler()
+# handler.setLevel(logging.INFO)
+
+# # Применяем цветной форматтер
+# formatter = ColoredFormatter("\n%(asctime)s - %(levelname)s: %(message)s\n")
+# handler.setFormatter(formatter)
+
+# log.addHandler(handler)
+
+# __all__ = ["log"]
+
+
 import logging
-
-# ANSI-коды цветов
-COLORS = {
-    "RESET": "\033[0m",  # Сброс цвета
-    "DEBUG": "\033[94m",  # Синий
-    "INFO": "\033[30m",  # сСерый
-    "WARNING": "\033[93m",  # Жёлтый
-    "ERROR": "\033[91m",  # Красный
-    "CRITICAL": "\033[41m",  # Красный фон
-}
-
-
-class ColoredFormatter(logging.Formatter):
-    def format(self, record) -> str:
-        """Форматирование лог-записи с учётом цвета"""
-
-        log_color = COLORS.get(record.levelname, COLORS["RESET"])
-        log_message = super().format(record)
-        return f"{log_color}{log_message}{COLORS['RESET']}"
-
 
 # Настраиваем логгер
 log = logging.getLogger()
@@ -28,10 +47,14 @@ log.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
 
-# Применяем цветной форматтер
-formatter = ColoredFormatter("\n%(asctime)s - %(levelname)s: %(message)s\n")
+# Простой формат без цветов и управляющих символов
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
 handler.setFormatter(formatter)
 
-log.addHandler(handler)
+# Добавляем хэндлер (только один раз)
+if not log.handlers:
+    log.addHandler(handler)
 
 __all__ = ["log"]
