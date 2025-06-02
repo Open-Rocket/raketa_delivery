b109462fbba0da528ae7f570b38dc95e253d3e81
RuslanChukavin <r66cha@gmail.com>
2025-06-02 17:04:17 +0300

    clear redis dev trash 2

--- src/confredis/__init__.py
+++ src/confredis/__init__.py
@@ -1,16 +1,10 @@
 from .redis_service import (
     RedisService,
-    create_redis_service,
     rediska,
-)
-from .redis_service_dev import (
-    rediska_dev,
 )
 
 
 __all__ = [
     "RedisService",
-    "create_redis_service",
     "rediska",
-    "rediska_dev",
 ]
